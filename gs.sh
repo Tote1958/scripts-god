1d105c8ef3a316516aad6a625713181f5ad733ab
Santino Beltran <s.beltran@alumno.un.edu.ar>
2023-06-26 02:38:18 -0300

    Version 2 gs.sh

--- gs.sh
+++ gs.sh
@@ -3,23 +3,20 @@
 for i in `ls`
 do
 	cd $i
-	git_stat_seg=$(git status | grep "Cambios no rastreados para el commit:" | wc -l)
-	git_stat_changes=$(git status | grep "Archivos sin seguimiento:" | wc -l)
+	git_stat_changes=$(git status | grep "Cambios no rastreados para el commit:" | wc -l)
+	git_stat_seg=$(git status | grep "Archivos sin seguimiento:" | wc -l)
 	if [ $git_stat_seg = 0 ] && [ $git_stat_changes = 0 ]
 	then
 		echo "$i esta actualizada"
 	else
-		if [ $git_stat_seg = 0 ]
+		if [ $git_stat_seg = 0 ] && [ $git_stat_changes -gt 0 ]
 		then
-			echo "$i no tiene archivos sin seguimiento"
-		else
-		echo "$i tiene archivos sin seguimiento"
-		fi
-		if [ $git_stat_changes = 0 ]
-		then
-			echo "$i no tiene archivos con cambios"
-		else
 			echo "$i tiene archivos con cambios"
+		elif [ $git_stat_seg -gt 0 ] && [ $git_stat_changes = 0 ]
+		then
+			echo "$i tiene archivos sin seguimiento"
+		else
+			echo "$i tiene archivos con cambios y archivos sin seguimiento"
 		fi
 	fi
 	cd ..
